841084b64ced0be50c270bbfd296fc2a61f951c7
Konrad <konradrozycki0@gmail.com>
2020-09-08 19:09:26 +0200

    update

--- public/js/custom-file-input.js
+++ public/js/custom-file-input.js
@@ -1,34 +1,27 @@
-/*
-	By Osvaldas Valutis, www.osvaldas.info
-	Available for use under the MIT License
-*/
+(function (document, window, index) {
+    var inputs = document.querySelectorAll(".inputfile");
+    Array.prototype.forEach.call(inputs, function (input) {
+        var label = input.nextElementSibling,
+            labelVal = label.innerHTML;
 
-'use strict';
+        input.addEventListener("change", function (e) {
+            var fileName = "";
+            if (this.files && this.files.length > 1)
+                fileName = (
+                    this.getAttribute("data-multiple-caption") || ""
+                ).replace("{count}", this.files.length);
+            else fileName = e.target.value.split("\\").pop();
 
-;( function ( document, window, index )
-{
-	var inputs = document.querySelectorAll( '.inputfile' );
-	Array.prototype.forEach.call( inputs, function( input )
-	{
-		var label	 = input.nextElementSibling,
-			labelVal = label.innerHTML;
+            if (fileName) label.querySelector("span").innerHTML = fileName;
+            else label.innerHTML = labelVal;
+        });
 
-		input.addEventListener( 'change', function( e )
-		{
-			var fileName = '';
-			if( this.files && this.files.length > 1 )
-				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-			else
-				fileName = e.target.value.split( '\\' ).pop();
-
-			if( fileName )
-				label.querySelector( 'span' ).innerHTML = fileName;
-			else
-				label.innerHTML = labelVal;
-		});
-
-		// Firefox bug fix
-		input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
-		input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
-	});
-}( document, window, 0 ));
+        // Firefox bug fix
+        input.addEventListener("focus", function () {
+            input.classList.add("has-focus");
+        });
+        input.addEventListener("blur", function () {
+            input.classList.remove("has-focus");
+        });
+    });
+})(document, window, 0);
