69c8b58b14d06ef1e35cf91c40aa4a30300e8a1e
Konrad <konradrozycki0@gmail.com>
2020-09-09 14:41:02 +0200

    update

--- public/js/custom-file-input.js
+++ public/js/custom-file-input.js
@@ -5,15 +5,16 @@
             labelVal = label.innerHTML;
 
         input.addEventListener("change", function (e) {
-            var fileName = "";
+            var file = "";
             if (this.files && this.files.length > 1)
-                fileName = (
+                file = (
                     this.getAttribute("data-multiple-caption") || ""
                 ).replace("{count}", this.files.length);
-            else fileName = e.target.value.split("\\").pop();
+            else file = e.target.value.split("\\").pop();
 
-            if (fileName) label.querySelector("span").innerHTML = fileName;
+            if (file) label.querySelector("span").innerHTML = file;
             else label.innerHTML = labelVal;
+            document.getElementById("file").value = "";
         });
 
         // Firefox bug fix
